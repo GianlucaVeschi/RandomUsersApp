836a113d41cbf167a3e4843ec0511bddef420603
Gianluca Veschi <sheckleeer@gmail.com>
2023-02-08 21:44:13 +0100

    remove unused constants

--- app/src/main/java/com/gianlucaveschi/randomusersapp/data/Constants.kt
+++ app/src/main/java/com/gianlucaveschi/randomusersapp/data/Constants.kt
@@ -2,14 +2,8 @@ package com.gianlucaveschi.randomusersapp.data
 
 object NetworkService {
     const val BASE_URL: String = "https://randomuser.me/api/"
-    const val ENDPOINT = ""
 }
 
 object DatabaseService {
     const val DATABASE_NAME: String = "users_app_db"
-}
-
-object Datasource {
-    const val NETWORK = "network"
-    const val CACHE = "cache"
 }
