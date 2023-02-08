3e3ed5c29b41efdfb829bc9021ea1e910c186490
Gianluca Veschi <sheckleeer@gmail.com>
2023-02-08 13:20:14 +0100

    Delete unused repository

--- app/src/test/java/com/gianlucaveschi/weatherapp/domain/repo/RandomUsersRepositoryTest.kt
+++ app/src/test/java/com/gianlucaveschi/weatherapp/domain/repo/RandomUsersRepositoryTest.kt
@@ -3,7 +3,8 @@ package com.gianlucaveschi.weatherapp.domain.repo
 import com.gianlucaveschi.randomusersapp.data.db.UsersDao
 import com.gianlucaveschi.randomusersapp.data.db.users.UserEntity
 import com.gianlucaveschi.randomusersapp.data.remote.RandomUsersService
-import com.gianlucaveschi.randomusersapp.data.repo.DatabaseUsersRepositoryImpl
+import com.gianlucaveschi.randomusersapp.data.remote.users.*
+import com.gianlucaveschi.randomusersapp.data.repo.UsersRepositoryImpl
 import com.gianlucaveschi.randomusersapp.domain.model.*
 import com.gianlucaveschi.randomusersapp.domain.repo.RandomUsersRepository
 import com.gianlucaveschi.randomusersapp.domain.util.Resource
@@ -13,8 +14,11 @@ import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import retrofit2.Response
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -24,13 +28,13 @@ class RandomUsersRepositoryTest : BaseJunitTest<RandomUsersRepository>() {
     private val api: RandomUsersService = mockk()
     private val dao: UsersDao = mockk()
 
-    override fun initSelf() = DatabaseUsersRepositoryImpl(
+    override fun initSelf() = UsersRepositoryImpl(
         api = api,
         dao = dao
     )
 
     @Test
-    fun `Given a successful database hit THEN return successful response`() = runTest {
+    fun `GIVEN a successful database hit THEN return successful response`() = runTest {
         coEvery {
             dao.getAllUsers()
         } returns listOf(
@@ -60,6 +64,106 @@ class RandomUsersRepositoryTest : BaseJunitTest<RandomUsersRepository>() {
         assertThat(result.data)
             .usingRecursiveComparison()
             .isEqualTo(mockedUsersList);
+    }
+
+    @Test
+    fun `GIVEN an empty database hit THEN fetch data from api`() = runTest {
+        coEvery { dao.getAllUsers() } returns listOf()
+        coEvery {
+            api.getUsers(
+                page = 1,
+                seed = "abc",
+                results = 20
+            )
+        } returns Response.success(
+            UsersApiResponse(
+                info = ApiResponseInfoApiModel(
+                    page = 1,
+                    results = 10,
+                    seed = "abc",
+                    version = "1.0"
+                ),
+                results = listOf(
+                    UserApiModel(
+                        cell = "123456789",
+                        dob = DateOfBirthApiModel(date = "2000-01-01T00:00:00.000Z", age = 23),
+                        email = "example@email.com",
+                        gender = "Male",
+                        id = IdApiModel(
+                            name = "1234567890",
+                            value = "1234567890"
+                        ),
+                        location = LocationApiModel(
+                            street = StreetApiModel(
+                                name = "",
+                                number = 0
+                            ),
+                            city = "",
+                            state = "",
+                            postcode = "",
+                            coordinates = CoordinatesApiModel(
+                                latitude = "latitude",
+                                longitude = "longitude"
+                            ),
+                            country = "",
+                            timezone = TimezoneApiModel(
+                                description = "description",
+                                offset = "offset"
+                            ),
+                        ),
+                        login = null,
+                        name = NameApiModel(
+                            title = "Mr",
+                            first = "John",
+                            last = "Doe"
+                        ),
+                        nat = "US",
+                        phone = "1234567890",
+                        picture = PictureApiModel(
+                            large = "https://example.com/large.jpg",
+                            medium = "https://example.com/medium.jpg",
+                            thumbnail = "https://example.com/thumbnail.jpg"
+                        ),
+                        registered = RegisteredApiModel(date = "2000-01-01T00:00:00.000Z", age = 23)
+                    )
+                )
+            )
+        )
+
+
+        val result: Resource<Users> = systemUnderTest.getUsers()
+
+        coVerify(exactly = 1) {
+            api.getUsers(
+                page = 1,
+                seed = "abc",
+                results = 20
+            )
+        }
+        assertThat(result.data)
+            .usingRecursiveComparison()
+            .isEqualTo(mockedUsersList);
+    }
+
+    @Test
+    fun `GIVEN an empty database hit AND an unsuccessful api call THEN return error`() = runTest {
+        coEvery { dao.getAllUsers() } returns listOf()
+        coEvery {
+            api.getUsers(
+                page = 1,
+                seed = "abc",
+                results = 20
+            )
+        } returns Response.error(
+            400,
+            "{\"key\":[\"somestuff\"]}"
+                .toResponseBody("application/json".toMediaTypeOrNull())
+        )
+
+        val result: Resource<Users> = systemUnderTest.getUsers()
+
+        assertThat(result)
+            .isInstanceOf(Resource.Error::class.java)
     }
 
     private companion object {
