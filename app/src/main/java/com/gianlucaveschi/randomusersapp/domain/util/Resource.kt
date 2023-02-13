2624030407e9563bfe352d2b6491ebc7a880b804
Gianluca Veschi <sheckleeer@gmail.com>
2023-02-13 18:23:03 +0100

    undo breaking change

--- app/src/main/java/com/gianlucaveschi/randomusersapp/domain/util/Resource.kt
+++ app/src/main/java/com/gianlucaveschi/randomusersapp/domain/util/Resource.kt
@@ -1,11 +1,10 @@
 package com.gianlucaveschi.randomusersapp.domain.util
 
-sealed interface Resource<T> {
-    class Success<T>(data: T) : Resource<T>
-    class Error<T>(message: String) : Resource<T>
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?): Resource<T>(data)
+    class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
 }
 
-// Improved version could be:
 //sealed interface Resource<T> {
 //    data class Success<T>(val data: T) : Resource<T>
 //    data class Error<T>(val t: Throwable) : Resource<T>
